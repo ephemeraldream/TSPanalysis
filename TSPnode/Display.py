0cefac125026b05472d56c0290ca628e6a709eac
PotatoInTheBox <ivangusachenko3@gmail.com>
2023-04-07 21:58:55 -0700

    not finished yet

---
+++ TSPnode/Display.py
@@ -0,0 +1,123 @@
+import tkinter as tk
+from typing import Callable
+import math
+
+
+def say_hello():
+    print("Hello, World!")
+
+
+class Node:
+    def __init__(self, x: float, y: float, name: str):
+        self.x: float = x
+        self.y: float = y
+        self.weight: float = -1
+        self.name: str = name
+
+    def generate_weight(self):
+        self.weight = math.sqrt((self.x - self.x) **
+                                2 + (self.y - self.y) ** 2)
+    pass
+
+
+class Edge:
+    def __init__(self, source: Node, destination: Node):
+        self.source: Node = source
+        self.destination: Node = destination
+
+    pass
+
+
+class Scene():
+    def __init__(self):
+        self.window = tk.Tk()
+        self.canvas = tk.Canvas(self.window, width=300, height=300)
+        self.nodes: dict[str, Node] = dict()
+        self.edges: dict[tuple[str, str], Edge] = dict()
+        self.solve_command = None
+
+        self.window.protocol("WM_DELETE_WINDOW", self.window.quit)
+
+        # Add a canvas to the window
+        self.canvas.pack()
+
+        # Draw three circles on the canvas
+        #canvas.create_oval(50, 50, 150, 150, fill="red")
+        #canvas.create_oval(100, 100, 200, 200, fill="green")
+        #canvas.create_oval(150, 150, 250, 250, fill="blue")
+
+        solve_button = tk.Button(
+            self.window, text="Solve", command=self.run_solve_command)
+        solve_button.pack()
+
+        start_node_text = tk.Text(self.window)
+        start_node_text.pack()
+
+        # Add a "Quit" button to the window
+        quit_button = tk.Button(self.window, text="Quit",
+                                command=self.window.quit)
+        quit_button.pack()
+
+    def mainloop(self):
+        self.window.mainloop()
+
+    def add_node(self, node: Node):
+        self.nodes[node.name] = node
+
+    def add_edge(self, edge: Edge):
+        self.edges[(edge.source.name, edge.destination.name)] = edge
+
+    def draw(self):
+        self.canvas.delete('all')
+        NODE_SIZE = 20
+        edge: Edge
+        for edge in self.edges.values():
+            if edge.source.name not in self.nodes:
+                raise Exception(
+                    f"Tried to create an edge with a source that does not exist ({edge})")
+            if edge.destination.name not in self.nodes:
+                raise Exception(
+                    f"Tried to create an edge with a destination that does not exist ({edge})")
+            self.canvas.create_line(
+                edge.source.x, edge.source.y, edge.destination.x, edge.destination.y)
+        node: Node
+        for node in self.nodes.values():
+            self.canvas.create_oval(node.x - NODE_SIZE/2, node.y - NODE_SIZE/2,
+                                    node.x + NODE_SIZE/2, node.y + NODE_SIZE/2, fill="white")
+
+    def run_solve_command(self):
+        if callable(self.solve_command):
+            self.solve_command()
+        else:
+            raise Exception(
+                "No callable \"solve_command\" present. Run assign_solve_command(command: Callable) to fix the issue.")
+
+    def assign_solve_command(self, command: Callable):
+        self.solve_command = command
+
+
+def main():
+    scene = Scene()
+    scene.assign_solve_command(solve)
+    nodeA = Node(100, 100, "a")
+    nodeB = Node(150, 100, "b")
+    nodeC = Node(100, 150, "c")
+    edgeAB = Edge(nodeA, nodeB)
+    scene.add_node(nodeA)
+    scene.add_node(nodeB)
+    scene.add_node(nodeC)
+
+    scene.add_edge(edgeAB)
+
+    scene.draw()
+    scene.mainloop()
+
+    print("Closed succesfully")
+
+
+def solve():
+    print("totally solved")
+
+
+if __name__ == "__main__":
+    main()
