7e6fef01115427581313562e80c8a003abb780eb
PotatoInTheBox <ivangusachenko3@gmail.com>
2023-04-08 02:24:49 -0700

    commented NodeManager

--- TSPnode/NodeManager.py
+++ TSPnode/NodeManager.py
@@ -11,12 +11,15 @@ class NodeManager:
         self.edges: dict[tuple[str, str], Edge] = dict()
         self.scene: Display.Scene
 
+    # add a node to the NodeManager
     def add_node(self, node: Node):
         self.nodes[node.name] = node
 
+    # add an edge to the NodeManager
     def add_edge(self, edge: Edge):
         self.edges[(edge.source.name, edge.destination.name)] = edge
 
+    # generate all possible edges between all nodes if not already existing
     def generate_all_edges(self):
         for fromNode in self.nodes.values():
             for toNode in self.nodes.values():
@@ -24,7 +27,9 @@ class NodeManager:
                     self.edges[
                         (fromNode.name, toNode.name)] = Edge(fromNode, toNode)
 
-    def generate_matrix(self):
+    # generate a matrix with the rows being source (along the y axis) and columns being the
+    # destination (along the x axis). Each cell contains the weight/destination to the other node.
+    def generate_matrix(self) -> 'list[list[float]]':
         sorted_nodes = sorted([node.name for node in self.nodes.values()])
         edge_matrix = [None]*len(sorted_nodes)
         for y in range(len(sorted_nodes)):
@@ -40,12 +45,12 @@ class NodeManager:
             edge_matrix[y] = row
         return edge_matrix
 
-    #matrix: 'list[list[float]]'
-
+    # convert the index into the node it represents
     def get_node_from_matrix(self, index: int) -> Node:
         sorted_nodes = sorted([node.name for node in self.nodes.values()])
         return sorted_nodes[index]
 
+    # convert an index pair into the edge it represents
     def get_edge_from_matrix(self, from_index: int, to_index: int) -> Edge:
         sorted_nodes = sorted([node.name for node in self.nodes.values()])
         key = (sorted_nodes[from_index], sorted_nodes[to_index])
@@ -54,14 +59,19 @@ class NodeManager:
         else:
             return None
 
+    # initialize the display by preparing the window (mainloop is needed for the window to show up)
     def init_display(self):
         self.scene = Display.Scene()
 
+    # begin the tkinter display mainloop (note: execution is permanently kept by the display at this point.
+    # Use event hooks to receive execute commands)
     def display_mainloop(self):
         self.scene.mainloop()
 
+    # draw/redraw all the node elements
     def draw(self):
         self.scene.draw_nodes(self.nodes, self.edges)
 
+    # pass in your custom function to be executed when a user clicks the "solve" button
     def assign_solve_command(self, command: Callable):
         self.scene.assign_solve_command(command)
