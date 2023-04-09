3c83f8fcde92016024287d5d2ae02bff0938923e
PotatoInTheBox <ivangusachenko3@gmail.com>
2023-04-08 22:12:54 -0700

    added solution

---
+++ TSPnode/BruteForceTSP.py
@@ -0,0 +1,34 @@
+import itertools
+from NodeManager import NodeManager
+from MatrixToolsTSP import calculate_cost
+
+
+def solve_brute_force(node_manager: NodeManager):
+    if len(node_manager.nodes) > 9:
+        raise Exception(
+            "ABORTING! Cannot feasably solve over 9 nodes using this approach in a timely manner!")
+    matrix = node_manager.generate_matrix()
+
+    num_nodes = len(matrix)
+    nodes = list(range(num_nodes))
+    best_path = None
+    best_cost = float('inf')
+
+    # Iterate through all possible permutations of nodes
+    for perm in itertools.permutations(nodes):
+        # Calculate the cost of the current path
+        cost = 0
+        for i in range(num_nodes):
+            cost += matrix[perm[i-1]][perm[i]]
+
+        # Update the best path and cost if this path is better
+        if cost < best_cost:
+            best_path = list(perm)
+            best_cost = cost
+
+    print(best_path)
+    print(calculate_cost(matrix, best_path))
+
+    print("Solved!")
+
+    return best_path
---
+++ TSPnode/NearestNeighborTSP.py
@@ -0,0 +1,39 @@
+
+import random
+import math
+from NodeManager import NodeManager
+from MatrixToolsTSP import calculate_cost
+# currently following the pdf below
+# http://160592857366.free.fr/joe/ebooks/ShareData/Heuristics%20for%20the%20Traveling%20Salesman%20Problem%20By%20Christian%20Nillson.pdf
+
+
+def solve_nearest_neighbor(node_manager: NodeManager):
+    matrix = node_manager.generate_matrix()
+    visited_cities = [False]*len(matrix)
+    path = []
+
+    # select a random city
+    start_city = random.randrange(0, len(matrix))
+    path.append(start_city)
+    visited_cities[start_city] = True
+
+    curr_city = start_city
+    while False in visited_cities:
+        # find nearest unvisited city and go there
+        nearest_unvisited_city = None
+        nearest_unvisited_weight = math.inf
+        for i in range(len(matrix)):
+            if curr_city != i and not visited_cities[i] and matrix[curr_city][i] < nearest_unvisited_weight:
+                nearest_unvisited_city = i
+        path.append(nearest_unvisited_city)
+        visited_cities[nearest_unvisited_city] = True
+
+        # are there any unvisited cities left? If yes, repeat step 2
+
+    # return to first city
+    path.append(start_city)
+
+    print(path)
+    print(calculate_cost(matrix, path))
+
+    print("Solved!")
