c08465040dc946ffff6a3e02e1cd605d6190c7d8
PotatoInTheBox <ivangusachenko3@gmail.com>
2023-04-09 00:11:50 -0700

    commenting and refactoring

--- TSPnode/BruteForceTSP.py
+++ TSPnode/BruteForceTSP.py
@@ -3,30 +3,77 @@ from NodeManager import NodeManager
 from MatrixToolsTSP import calculate_cost
 import MatrixToolsTSP
 
+
 def solve(node_manager: NodeManager) -> None:
+    """Solve the travelling salesman problem using a brute-force approach
+    (assuming all nodes have edges to all other nodes). Update the result to
+    the GUI.
+
+    Because this is a brute-force algorithm it will calculate the distance of
+    every possible permutation of paths. Thus, this algorithm will run O(n!)
+    operations, where n is the number of nodes. Space complexity will be
+    O(n) as we will be generating a path of n length.
+    
+    tl;dr
+        Time complexity: O(n!)
+        Space complexity: O(n)
+
+    Args:
+        node_manager (NodeManager): Needs a NodeManager to be passed as the
+        function will need to pull matrix data and then update the graph with
+        the solution.
+
+    Raises:
+        Exception: When attempting to solve a matrix bigger than 9
+        (it would take too long).
+    """
+    # Check if the number of nodes in the NodeManager object is greater than 9.
+    # If it is, raise an exception and print a message.
     if len(node_manager.nodes) > 9:
         raise Exception(
-            "ABORTING! Cannot feasably solve over 9 nodes using this approach in a timely manner!")
+            "ABORTING! Cannot feasably solve over 9 nodes \
+                using this approach in a timely manner!")
+
+    # Generate a matrix representing the distances 
+    # between nodes in the NodeManager object.
     matrix = node_manager.generate_matrix()
 
+    # Get the number of nodes in the matrix.
     num_nodes = len(matrix)
+
+    # Create a list of node indices.
     nodes = list(range(num_nodes))
+
+    # Initialize variables for storing the best path and its cost.
     best_path = None
     best_cost = float('inf')
 
-    # Iterate through all possible permutations of nodes
+    # Iterate through all possible permutations of the nodes.
     for perm in itertools.permutations(nodes):
-        # Calculate the cost of the current path
+        # Initialize a variable for storing the cost of the current path.
         cost = 0
+
+        # Calculate the cost of the current path.
         for i in range(num_nodes):
             cost += matrix[perm[i-1]][perm[i]]
 
-        # Update the best path and cost if this path is better
+        # Update the best path and its cost if this path is better.
         if cost < best_cost:
             best_path = list(perm)
             best_cost = cost
 
+    # Print the best path.
     print(best_path)
-    print(calculate_cost(matrix, best_path))
+
+    # Calculate and print the cost of the best path.
+    cost = calculate_cost(matrix, best_path)
+    print(cost)
+    
+    # Print a comparison between the best known solution and the current solution
+    print(MatrixToolsTSP.compare_best_solution(node_manager.best_solution_weight, cost))
+
+    # Highlight and draw the best path in the NodeManager object.
     MatrixToolsTSP.highlight_and_draw(node_manager, best_path)
+
+    # Print a message indicating that the problem has been solved.
     print("Solved!")
--- TSPnode/NearestNeighborTSP.py
+++ TSPnode/NearestNeighborTSP.py
@@ -8,19 +8,57 @@ import MatrixToolsTSP
 # http://160592857366.free.fr/joe/ebooks/ShareData/Heuristics%20for%20the%20Traveling%20Salesman%20Problem%20By%20Christian%20Nillson.pdf
 
 
+# This function takes a NodeManager object as an argument and returns None.
 def solve(node_manager: NodeManager) -> None:
+    """Solve the travelling salesman problem using a nearest-neighbor approach
+    (assuming all nodes have edges to all other nodes). Update the result to
+    the GUI.
+
+    The Nearest Neighbor approach is very lazy in which it repeatively chooses
+    the shortest node at every step. The steps are: pick random node,
+    repeatively pick the shortest unvisited node, complete the circuit. This
+    can sometimes give poor results but generally does better than simply
+    randomly picking a permutation as a path. It uses a space complexity of
+    O(n) to store a path and unvisited nodes.
+    
+    This approach is O(n). As it only has to keep picking the shortest path n
+    times. The current implementation however isn't quite O(n) because it
+    searches the entire node for the shortest next node at every step of the
+    path (making it O(n^2)). This, however, can be mitigated by using a
+    non-comparison sorting algorithm and appropriate data structures (making
+    it O(n) again). It's not currently implemented as it adds complexity
+    taking away from the main focus of the nearest neighbor algorithm.
+    
+    tl;dr
+        Time complexity: O(n) (currently though O(n^2))
+        Space complexity: O(n)
+
+    Args:
+        node_manager (NodeManager): Needs a NodeManager to be passed as the
+        function will need to pull matrix data and then update the graph with
+        the solution.
+    """
+    # Generate a matrix representing the distances between nodes in the NodeManager object.
     matrix = node_manager.generate_matrix()
+
+    # Create a list of boolean values representing whether each city has been visited or not.
     visited_cities = [False]*len(matrix)
+
+    # Create an empty list for storing the path.
     path = []
 
-    # select a random city
+    # Select a random city to start the path.
     start_city = random.randrange(0, len(matrix))
     path.append(start_city)
     visited_cities[start_city] = True
 
+    # Initialize a variable for storing the current city.
     curr_city = start_city
+
+    # Repeat the following steps until all cities have been visited.
     while False in visited_cities:
-        # find nearest unvisited city and go there
+
+        # Find the nearest unvisited city to the current city.
         nearest_unvisited_city = None
         nearest_unvisited_weight = math.inf
         for i in range(len(matrix)):
@@ -29,13 +67,21 @@ def solve(node_manager: NodeManager) -> None:
         path.append(nearest_unvisited_city)
         visited_cities[nearest_unvisited_city] = True
 
-        # are there any unvisited cities left? If yes, repeat step 2
+        # If there are any unvisited cities left, repeat the previous step.
 
-    # return to first city
-    # node: omitted but is currently still handled by node_manager (having a duplicate end and start)
-    # path.append(start_city)
 
+    # Print the path.
     print(path)
-    print(calculate_cost(matrix, path))
+
+    # Calculate and print the cost of the path.
+    cost = calculate_cost(matrix, path)
+    print(cost)
+    
+    # Print a comparison between the best known solution and the current solution
+    print(MatrixToolsTSP.compare_best_solution(node_manager.best_solution_weight, cost))
+
+    # Highlight and draw the path in the NodeManager object.
     MatrixToolsTSP.highlight_and_draw(node_manager, path)
+
+    # Print a message indicating that the problem has been solved.
     print("Solved!")
