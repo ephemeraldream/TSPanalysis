fb2d88f7e1ae1949d5672826bfc9cf452e0c9f51
PotatoInTheBox <ivangusachenko3@gmail.com>
2023-04-08 17:24:35 -0700

    cleaned up display, preparing to add more algo

--- TSPnode/Display.py
+++ TSPnode/Display.py
@@ -7,31 +7,33 @@ from Edge import Edge
 class Scene():
     def __init__(self):
         self.window = tk.Tk()
-        self.canvas = tk.Canvas(self.window, width=300, height=300)
-
+        self.canvas = tk.Canvas(self.window, width=400, height=400)
+        self.SOLVE_OPTIONS = ["Heuristic/Naive", "Option 2", "Option 3"]
         self.solve_command = None
 
         self.window.protocol("WM_DELETE_WINDOW", self.window.quit)
 
         # Add a canvas to the window
         self.canvas.pack()
+        
+        # Create the form at the bottom
+        form_frame = tk.Frame(self.window)
+        form_frame.pack(side=tk.BOTTOM)
 
-        # Draw three circles on the canvas
-        #canvas.create_oval(50, 50, 150, 150, fill="red")
-        #canvas.create_oval(100, 100, 200, 200, fill="green")
-        #canvas.create_oval(150, 150, 250, 250, fill="blue")
+        # Create the two text boxes
+        text_box1 = tk.Entry(form_frame, width=10)
+        text_box1.pack(side=tk.LEFT, padx=5, pady=5)
+        text_box2 = tk.Entry(form_frame, width=10)
+        text_box2.pack(side=tk.LEFT, padx=5, pady=5)
 
-        solve_button = tk.Button(
-            self.window, text="Solve", command=self.run_solve_command)
-        solve_button.pack()
+        # Create the submit button
+        submit_button = tk.Button(form_frame, text="Solve", command=self.run_solve_command)
+        submit_button.pack(side=tk.LEFT, padx=5, pady=5)
 
-        start_node_text = tk.Text(self.window)
-        start_node_text.pack()
-
-        # Add a "Quit" button to the window
-        quit_button = tk.Button(self.window, text="Quit",
-                                command=self.window.quit)
-        quit_button.pack()
+        # Create the dropdown button
+        selected_option = tk.StringVar(value=self.SOLVE_OPTIONS[0])
+        dropdown_menu = tk.OptionMenu(form_frame, selected_option, *self.SOLVE_OPTIONS)
+        dropdown_menu.pack(side=tk.LEFT, padx=5, pady=5)
 
     def mainloop(self):
         self.window.mainloop()
@@ -47,12 +49,23 @@ class Scene():
             if edge.destination.name not in nodes:
                 raise Exception(
                     f"Tried to create an edge with a destination that does not exist ({edge})")
+            color = None
+            if edge.highlight:
+                color = "red"
+            else:
+                color = "black"
             self.canvas.create_line(
-                edge.source.x, edge.source.y, edge.destination.x, edge.destination.y)
+                edge.source.x, edge.source.y, edge.destination.x, edge.destination.y, fill=color)
         node: Node
         for node in nodes.values():
+            color = None
+            if node.highlight:
+                color = "red"
+            else:
+                color = "black"
             self.canvas.create_oval(node.x - NODE_SIZE/2, node.y - NODE_SIZE/2,
-                                    node.x + NODE_SIZE/2, node.y + NODE_SIZE/2, fill="white")
+                                    node.x + NODE_SIZE/2, node.y + NODE_SIZE/2, fill="white", outline=color)
+            self.canvas.create_text(node.x, node.y, text=node.name)
 
     def run_solve_command(self):
         if callable(self.solve_command):
