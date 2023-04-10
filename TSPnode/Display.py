4d64ebb44c7a23c3be4c38a7ff8537ca953b29c2
PotatoInTheBox <ivangusachenko3@gmail.com>
2023-04-10 00:39:27 -0700

    organized and remove non-display functions

--- TSPnode/Display.py
+++ TSPnode/Display.py
@@ -4,78 +4,74 @@ from Node import Node
 from Edge import Edge
 
 
-class Scene():
+class Display():
     def __init__(self):
         self.window = tk.Tk()
         self.window.title("TSP Visual")
-        self.camera_x = 0
-        self.camera_y = 0
-        self.last_mouse_x = 0
-        self.last_mouse_y = 0
-        self.camera_width = 800
-        self.camera_height = 600
-        self.zoom = 1
-        self.canvas = tk.Canvas(
-            self.window, width=self.camera_width, height=self.camera_height)
-        self.canvas.configure(scrollregion=(0, 0, self.camera_width, self.camera_height), yscrollincrement='1', xscrollincrement='1')
-        self.canvas.configure(background='white')
 
-        self.canvas.bind("<MouseWheel>", self.scroll)
-        self.canvas.bind("<B1-Motion>", self.drag_camera)
-        self.canvas.bind("<ButtonPress>", self.start_drag_camera)
-        
-        
-
-        self.run_solve_command = None
-        self.solve_commands = None
-
-        self.window.protocol("WM_DELETE_WINDOW", self.window.quit)
-
-        # Add a canvas to the window
-        self.canvas.pack()
-        
-        # Create the form at the bottom
-        form_frame = tk.Frame(self.window)
-        form_frame.pack(side=tk.BOTTOM)
-        
-        # Create the submit button
-        self.submit_button = tk.Button(form_frame, text="Solve", command=None)
-        self.submit_button.pack(side=tk.LEFT, padx=5, pady=5)
-
-        self.distance_result = tk.Label(form_frame, text="Hello, world!", width=40)
-        self.distance_result.pack(side=tk.LEFT, padx=5, pady=5)
-        
-        # Create the dropdown button
-        self.selected_option = tk.StringVar(value="Select Algo")
-        self.dropdown_menu = tk.OptionMenu(
-            form_frame, self.selected_option, "Select Algo")
-        self.dropdown_menu.config(width=30)
-        self.dropdown_menu.pack(side=tk.LEFT, padx=5, pady=5)
-
-        self.show_unhighlighted_edges = True
-        self.toggle_highlight = tk.Button(form_frame, text="Toggle unhighlighted edges", command=self.toggle_unhighlighted_edges)
-        self.toggle_highlight.pack(side=tk.LEFT, padx=5, pady=5)
-
-        
-
-        
-        
-        self.nodes = None
-        self.edges = None
+        self._instantiate_canvas()
+        self._instantiate_events()
+        self._instantiate_bottom_form()
+        self._instantiate_solve_button()
+        self._instantiate_weight_label()
+        self._instantiate_algorithm_dropdown()
+        self._instantiate_highlight_toggle()
 
     def mainloop(self):
         self.window.mainloop()
-    
-    def toggle_unhighlighted_edges(self):
-        self.show_unhighlighted_edges = not self.show_unhighlighted_edges
-        self.draw_nodes(self.nodes, self.edges)
 
-    def scroll(self, event):
-        
+    def _instantiate_canvas(self):
+        self.last_mouse_x = 0
+        self.last_mouse_y = 0
+        self.viewport_width = 800
+        self.viewport_height = 600
+        self.zoom = 1
+        self.canvas = tk.Canvas(
+            self.window, width=self.viewport_width, height=self.viewport_height)
+        # scroll region allows for panning to be possible
+        self.canvas.configure(scrollregion=(
+            0, 0, self.viewport_width, self.viewport_height), yscrollincrement='1', xscrollincrement='1')
+        self.canvas.configure(background='white')
+        self.canvas.pack()
+
+    def _instantiate_events(self):
+        self.canvas.bind("<MouseWheel>", self._scroll_event)
+        self.canvas.bind("<B1-Motion>", self._drag_camera_event)
+        self.canvas.bind("<ButtonPress>", self._start_drag_camera_event)
+        self.window.protocol("WM_DELETE_WINDOW", self.window.quit)
+
+    def _instantiate_bottom_form(self):
+        self._form_frame = tk.Frame(self.window)
+        self._form_frame.pack(side=tk.BOTTOM)
+
+    def _instantiate_solve_button(self):
+        self.submit_button = tk.Button(
+            self._form_frame, text="Solve", command=None)
+        self.submit_button.pack(side=tk.LEFT, padx=5, pady=5)
+
+    def _instantiate_weight_label(self):
+        self.distance_result = tk.Label(
+            self._form_frame, text="Hello, world!", width=40)
+        self.distance_result.pack(side=tk.LEFT, padx=5, pady=5)
+
+    def _instantiate_algorithm_dropdown(self):
+        self.selected_option = tk.StringVar(value="Select Algo")
+        self.dropdown_menu = tk.OptionMenu(
+            self._form_frame, self.selected_option, "Select Algo")
+        self.dropdown_menu.config(width=30)
+        self.dropdown_menu.pack(side=tk.LEFT, padx=5, pady=5)
+
+    def _instantiate_highlight_toggle(self):
+        self.show_unhighlighted_edges = True
+        self.toggle_highlight = tk.Button(
+            self._form_frame, text="Toggle unhighlighted edges", command=self.toggle_unhighlighted_edges)
+        self.toggle_highlight.pack(side=tk.LEFT, padx=5, pady=5)
+
+    def _scroll_event(self, event):
         x_pos, _ = self.canvas.xview()
-        x_pos *= self.camera_width
+        x_pos *= self.viewport_width
         y_pos, _ = self.canvas.yview()
-        y_pos *= self.camera_height
+        y_pos *= self.viewport_height
         new_x_pos = None
         new_y_pos = None
         offset_x = None
@@ -84,8 +80,8 @@ class Scene():
             # shrink
             if self.zoom < 64:
                 self.zoom *= 2
-                viewport_width = self.camera_width / self.zoom
-                viewport_height = self.camera_height / self.zoom
+                viewport_width = self.viewport_width / self.zoom
+                viewport_height = self.viewport_height / self.zoom
                 offset_x = viewport_width/2
                 offset_y = viewport_height/2
                 new_x_pos = x_pos + offset_x
@@ -94,8 +90,8 @@ class Scene():
                 return
         else:
             # grow
-            viewport_width = self.camera_width / self.zoom
-            viewport_height = self.camera_height / self.zoom
+            viewport_width = self.viewport_width / self.zoom
+            viewport_height = self.viewport_height / self.zoom
             if self.zoom > 1:
                 self.zoom /= 2
                 offset_x = viewport_width/2
@@ -104,55 +100,64 @@ class Scene():
                 new_y_pos = y_pos - offset_y
             else:
                 return
-            
+
         # zoom towards center
-        
+
         self.canvas.configure(scrollregion=(0,
                                             0,
-                                            self.camera_width * self.zoom,
-                                            self.camera_height * self.zoom))
-        
-        self.draw_nodes(self.nodes, self.edges)
-        self.canvas.xview_moveto(new_x_pos/self.camera_width)
-        self.canvas.yview_moveto(new_y_pos/self.camera_height)
-        
-    def start_drag_camera(self, event):
+                                            self.viewport_width * self.zoom,
+                                            self.viewport_height * self.zoom))
+
+        self.draw_nodes(self.node_cache, self.edge_cache)
+        self.canvas.xview_moveto(new_x_pos/self.viewport_width)
+        self.canvas.yview_moveto(new_y_pos/self.viewport_height)
+
+    def _start_drag_camera_event(self, event):
         if event.state == 0x0008 and event.num == 1:  # Left mouse button
             self.last_mouse_x = event.x
             self.last_mouse_y = event.y
-    
-    def drag_camera(self, event):
+
+    def _drag_camera_event(self, event):
         # Calculate the difference in mouse position since last event
         dx = event.x - self.last_mouse_x
         dy = event.y - self.last_mouse_y
-        
-        # Update the camera position based on the mouse movement
-        #self.camera_x -= dx
-        #self.camera_y -= dy
-        
+
         # Redraw the canvas with the updated camera position
         self.canvas.xview_scroll(-dx, "units")
         self.canvas.yview_scroll(-dy, "units")
-        
-        x_pos, _ = self.canvas.xview()
-        y_pos, _ = self.canvas.yview()
-        
+
         # Remember the current mouse position for the next event
         self.last_mouse_x = event.x
         self.last_mouse_y = event.y
 
+    def _cam_transform_x(self, x: int):
+        return x * self.zoom
 
-    def cam_transform_x(self, x: int):
-        return (x) * self.zoom
-    def cam_transform_y(self, y: int):
-        return (y) * self.zoom
+    def _cam_transform_y(self, y: int):
+        return y * self.zoom
+    
+    def toggle_unhighlighted_edges(self):
+        """Toggle whether or not the unhighlighted edges should be rendered.
+            (An event will call this function).
+        """
+        self.show_unhighlighted_edges = not self.show_unhighlighted_edges
+        self.draw_nodes(self.node_cache, self.edge_cache)
+
     def draw_nodes(self, nodes: 'dict[str, Node]', edges: 'dict[tuple[str, str], Edge]'):
-        self.nodes = nodes
-        self.edges = edges
+        """Draw given nodes and edges. Nodes will be on top, next will be the
+            weight text, next the highlighted edges, and finally the
+            unhighlighted edges will be drawn.
 
-        xt = self.cam_transform_x
-        yt = self.cam_transform_y
-        
+        Args:
+            nodes (dict[str, Node]): Node to draw
+            edges (dict[tuple[str, str], Edge]): Edge to draw
+        """
+        self.node_cache = nodes # cache for quickly redrawing scene when zooming
+        self.edge_cache = edges # cache for quickly redrawing scene when zooming
+
+        xt = self._cam_transform_x
+        yt = self._cam_transform_y
+
         self.canvas.delete('all')
         NODE_SIZE = 26
         edge: Edge
@@ -177,10 +182,10 @@ class Scene():
             edsty = edge.destination.y
             if edge.highlight:
                 self.canvas.create_line(
-                    xt(esrcx), yt(esrcy), 
+                    xt(esrcx), yt(esrcy),
                     xt(edstx), yt(edsty), fill="black", width=5)
                 self.canvas.create_line(
-                    xt(esrcx), yt(esrcy), 
+                    xt(esrcx), yt(esrcy),
                     xt(edstx), yt(edsty), fill="red", width=3)
         for edge in edges.values():
             esrcx = edge.source.x
@@ -208,10 +213,16 @@ class Scene():
             self.canvas.create_text(
                 xt(node.x), yt(node.y), text=node.name)
         self.distance_result
-        
-        
 
-    def assign_solve_command(self, command: Callable, name: str):
-        self.submit_button.configure(command=self.run_solve_command)
-        self.dropdown_menu["menu"].add_command(
-            label=name, command=tk._setit(self.selected_option, name))
+    def assign_solve_options(self, commands_str: 'list[str]'):
+        """Set all dropdown options from a given string array.
+
+        Args:
+            commands_str (list[str]): A list containing strings which represent
+            the names of each algorithm. (Each one should be unique)
+        """
+        menu = self.dropdown_menu["menu"]
+        menu.delete(0, "end")
+        for command_str in commands_str:
+            menu.add_command(label=command_str, command=tk._setit(
+                self.selected_option, command_str))
