8f4c4eead9f32998dce6e32feb842c238df1f4b8
PotatoInTheBox <ivangusachenko3@gmail.com>
2023-04-10 00:39:00 -0700

    removed responsibility away from main

--- TSPnode/Main.py
+++ TSPnode/Main.py
@@ -1,26 +1,10 @@
-from NodeManager import NodeManager
-import NearestNeighborTSP
-import BruteForceTSP
-import MatrixToolsTSP
-import SimulatedAnnealing
-
-node_manager = NodeManager()
-
+from TSPApplication import TSPApplication
 
 def main():
-    node_manager.init_display()
     
-    node_manager.assign_solve_command(
-        NearestNeighborTSP.solve, "Nearest Neighbor")
-    node_manager.assign_solve_command(BruteForceTSP.solve, "Brute force")
-    node_manager.assign_solve_command(SimulatedAnnealing.simulated_annealing, "Simulated Annealing")
-
-    node_manager.generate_random_graph(0, 200)
-    #node_manager.generate_graph(MatrixToolsTSP.graph20)
-    #node_manager.best_solution_weight = MatrixToolsTSP.graph7_best_distance
-
-    node_manager.draw()
-    node_manager.display_mainloop()
+    tsp_app = TSPApplication()
+    
+    tsp_app.enter_mainloop()
 
     print("Closed succesfully")
 
